8cc3b1962bb4454d62b8300d21a8a92511752e53
heikkinenlaura <77092247+heikkinenlaura@users.noreply.github.com>
2021-08-25 11:41:08 +0300

    pausetest

--- TheSchoolStrippersGame/Assets/Scripts/UI/ButtonHandler.cs
+++ TheSchoolStrippersGame/Assets/Scripts/UI/ButtonHandler.cs
@@ -25,34 +25,22 @@ public class ButtonHandler : MonoBehaviour
         GameObject.Find("DotSpawner").GetComponent<DotSpawner>().enabled = false;
         GameObject.Find("CollectingDotsSpawner").GetComponent<DotSpawner>().enabled = false;
 
-        GameObject.Find("BlueDot(Clone)").GetComponent<MovingDots>().enabled = false;
-        GameObject.Find("YellowDot(Clone)").GetComponent<MovingDots>().enabled = false;
-        GameObject.Find("RedDot(Clone)").GetComponent<MovingDots>().enabled = false;
-        GameObject.Find("GreenDot(Clone)").GetComponent<MovingDots>().enabled = false;
-
-        GameObject.Find("Dot1(Clone)").transform.GetChild(0).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("Dot2(Clone)").transform.GetChild(0).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("Dot3(Clone)").transform.GetChild(0).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("Dot4(Clone)").transform.GetChild(0).GetComponent<DotSpawnPoint>().enabled = false;
-
-        GameObject.Find("DotPattern1(Clone)").transform.GetChild(0).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("DotPattern1(Clone)").transform.GetChild(1).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("DotPattern2(Clone)").transform.GetChild(0).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("DotPattern2(Clone)").transform.GetChild(1).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("DotPattern3(Clone)").transform.GetChild(0).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("DotPattern3(Clone)").transform.GetChild(1).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("DotPattern4(Clone)").transform.GetChild(0).GetComponent<DotSpawnPoint>().enabled = false;
-        GameObject.Find("DotPattern4(Clone)").transform.GetChild(1).GetComponent<DotSpawnPoint>().enabled = false;
+        //GameObject.Find("BlueDot(Clone)").GetComponent<MovingDots>().enabled = false;
+        //GameObject.Find("YellowDot(Clone)").GetComponent<MovingDots>().enabled = false;
+        //GameObject.Find("RedDot(Clone)").GetComponent<MovingDots>().enabled = false;
+        //GameObject.Find("GreenDot(Clone)").GetComponent<MovingDots>().enabled = false;
     }
 
     public void ContinueGame()
     {
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
         GameObject.Find("DotSpawner").GetComponent<DotSpawner>().enabled = true;
-        GameObject.FindGameObjectWithTag("BlueDot").GetComponent<MovingDots>().enabled = true;
-        GameObject.FindGameObjectWithTag("YellowDot").GetComponent<MovingDots>().enabled = true;
-        GameObject.FindGameObjectWithTag("RedDot").GetComponent<MovingDots>().enabled = true;
-        GameObject.FindGameObjectWithTag("GreenDot").GetComponent<MovingDots>().enabled = true;
+        GameObject.Find("CollectingDotsSpawner").GetComponent<DotSpawner>().enabled = true;
+
+        //GameObject.FindGameObjectWithTag("BlueDot").GetComponent<MovingDots>().enabled = true;
+        //GameObject.FindGameObjectWithTag("YellowDot").GetComponent<MovingDots>().enabled = true;
+        //GameObject.FindGameObjectWithTag("RedDot").GetComponent<MovingDots>().enabled = true;
+        //GameObject.FindGameObjectWithTag("GreenDot").GetComponent<MovingDots>().enabled = true;
     }
 
     private void Update()
