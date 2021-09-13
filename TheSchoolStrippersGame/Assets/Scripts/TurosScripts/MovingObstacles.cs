9b73040106d306dc9f41c4cf136ba02fa3260afb
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-09-13 10:19:52 +0300

    Update for obstacle movement
    Added speed stages for obstacle movement

--- TheSchoolStrippersGame/Assets/Scripts/TurosScripts/MovingObstacles.cs
+++ TheSchoolStrippersGame/Assets/Scripts/TurosScripts/MovingObstacles.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class MovingObstacles : MonoBehaviour
 {
-    [SerializeField] float speed = 2f;
+    [SerializeField] float speed = 2f, speedSlow = 1f;
+    bool isSwimmingFast = true;
+    bool isSimmingSlow = false;
 
     private void Update()
     {
-        transform.Translate(Vector2.right * -speed * Time.deltaTime);
+        if (isSwimmingFast)
+        {
+            StartCoroutine(SwimmingFast());
+            //transform.Translate(Vector2.right * -speed * Time.deltaTime);
+        }
+
+        if(isSimmingSlow && isSwimmingFast == false)
+        {
+            StartCoroutine(SwimmingSlow());
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,5 +41,27 @@ public class MovingObstacles : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    IEnumerator SwimmingFast()
+    {
+        transform.Translate(Vector2.right * -speed * Time.deltaTime);
+
+        yield return new WaitForSeconds(2);
+
+        isSwimmingFast = false;
+        isSimmingSlow = true;
+
+    }
+
+    IEnumerator SwimmingSlow()
+    {
+        transform.Translate(Vector2.right * -speedSlow * Time.deltaTime);
+
+        yield return new WaitForSeconds(1);
+
+        isSimmingSlow = false;
+        isSwimmingFast = true;
+        
     }
 }
