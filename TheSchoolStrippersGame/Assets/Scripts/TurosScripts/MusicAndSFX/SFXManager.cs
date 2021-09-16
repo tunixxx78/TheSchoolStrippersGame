db14ddbd5570e644cc29cdb58a5246fb901bffb8
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-09-16 12:00:46 +0300

    SFXManager script modified

--- TheSchoolStrippersGame/Assets/Scripts/TurosScripts/MusicAndSFX/SFXManager.cs
+++ TheSchoolStrippersGame/Assets/Scripts/TurosScripts/MusicAndSFX/SFXManager.cs
@@ -6,19 +6,6 @@ using UnityEngine.Audio;
 public class SFXManager : MonoBehaviour
 {
     public AudioSource swim, breakingShip, collecting1, collecting2;
-    public static SFXManager sfxInstance;
-
-    private void Awake()
-    {
-        if(sfxInstance != null && sfxInstance != this)
-        {
-            Destroy(this.gameObject);
-            return;
-        }
-
-        sfxInstance = this;
-        DontDestroyOnLoad(this);
-    }
 
     public void Swim()
     {
